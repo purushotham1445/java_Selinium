8d89e287224f5ea748a160e8409e2324baeeab24
mallikarjunaboya203 <mallikarjunaboya203@gmail.com>
2023-09-15 10:55:30 +0530

    This is first commit codde for Mallikarjuna

---
+++ src/main/java/PusrshothamAndMalli/PusrshothamAndMalli/App.java
@@ -0,0 +1,13 @@
+package PusrshothamAndMalli.PusrshothamAndMalli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/PusrshothamAndMalli/PusrshothamAndMalli/SampleClass.java
@@ -0,0 +1,21 @@
+package PusrshothamAndMalli.PusrshothamAndMalli;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+
+		
+		
+		if(System.out.printf("WeloCome to Purshotham and Mallikarjuna Creation \n")==null) {
+			
+		}
+	if(System.out.append("WeloCome to Purshotham and Mallikarjuna Creation \n")==null) {
+			
+		}
+	if(System.out.append("WeloCome to Purshotham and Mallikarjuna Creation ").equals(null)) {
+		
+	}
+	
+	}
+
+}
